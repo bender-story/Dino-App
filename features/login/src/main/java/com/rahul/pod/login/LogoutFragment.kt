0feffe7742bbdccda440f3170441781983f6b84c
Rahul <rchadalawada@sg.palo-it.com>
2021-04-18 19:01:37 +0800

    Add Logout Page

---
+++ features/login/src/main/java/com/rahul/pod/login/LogoutFragment.kt
@@ -0,0 +1,25 @@
+package com.rahul.pod.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LogoutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LogoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logout, container, false)
+    }
+
+}
